c86563cbd9705eca2cf1211ec23cd816aba3beb9
Taranchuk <nfhfyxer@gmail.com>
2025-03-01 14:56:52 +0200

    add mouse cell coordinations

---
+++ 1.5/Source/AquilesCore/AquilesCoreMod.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace AquilesCore
+{
+    [StaticConstructorOnStartup]
+    public static class AquilesCoreMod
+    {
+        static AquilesCoreMod()
+        {
+            new Harmony("AquilesCoreMod").PatchAll();
+        }
+    }
+
+    [HarmonyPatch(typeof(MouseoverUtility), "GetGlowLabelByValue")]
+    public static class MouseoverUtility_GetGlowLabelByValue_Patch
+    {
+        public static void Postfix(ref string __result)
+        {
+            var map = Find.CurrentMap;
+            if (map != null)
+            {
+                IntVec3 cell = UI.MouseCell();
+                __result += $" ({cell.x}, {cell.z})";
+            }
+        }
+    }
+}
